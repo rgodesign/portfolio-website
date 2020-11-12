0ef66491250b0ddcb01f9f71f712b5ade03f4861
rgodesign <73262347+rgodesign@users.noreply.github.com>
2020-11-11 19:13:28 -0500

    Hamburger menu
    Added Javascript and styling for a hamburger menu at small breakpoint

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log("Hello specifically to this console on my portfolio.")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += 'responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
